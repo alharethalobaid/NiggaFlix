1461931585e42757a2a52e365c3f1611fff17af0
alhareth alobaid <alhareth.alobaid@gmail.com>
2026-05-08 08:26:16 +0300

    Create P3.tsx

---
+++ src/routes/P3.tsx
@@ -0,0 +1,19 @@
+import Episodes from "~/components/Episodes";
+import Roms from "~/components/Roms";
+export default function P3() {
+  return (
+    <main class="mx-auto mb-auto p-4 text-center">
+      
+      <h1 class="text-6xl text-primary font-thin uppercase my-16">
+       
+      </h1>
+
+      <Roms/>
+      <Episodes/>
+      <p class="mt-8">
+       
+      </p>
+      
+    </main>
+  );
+}
