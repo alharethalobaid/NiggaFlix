cc55fa35ae076f76465e3377ef931c926479c042
alhareth alobaid <alhareth.alobaid@gmail.com>
2026-05-18 11:44:11 +0300

    Update [...404].tsx

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -5,9 +5,9 @@ export default function NotFound() {
   return (
     <main class="mx-auto mb-auto p-4 text-center">
       
-      <h1 class="text-6xl text-primary font-thin uppercase my-16">
+      <h1 class="text-6xl text-primary text-red-500 font-thin uppercase my-16">
         404 <br />
-        Page Nigger Found!
+        Page Not Found!
       </h1>
                   
     </main>
